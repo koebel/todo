b316b77a4ee194db48e4f017008269d39100dec1
Kathrin Koebel <kathrin@koebel.ch>
2023-06-15 18:27:19 +0545

    add second scenario

--- tests/acceptance/features/todo.feature
+++ tests/acceptance/features/todo.feature
@@ -7,3 +7,10 @@ Feature: todo
 		Given a user has navigated to the homepage
 		When the user adds "new task" to the todo list using the webUI
 		Then card "new task" should be displayed on the webUI
+
+	Scenario: mark todo list item as completed
+		Given a user has navigated to the homepage 
+		And a card "new task" has been added to the todo list
+		When the user marks the card "new task" as completed using the webUI
+		Then the card "new task" should be labeled as completed on the webUI
+		
--- tests/acceptance/stepDefinitions/todoContext.js
+++ tests/acceptance/stepDefinitions/todoContext.js
@@ -19,7 +19,12 @@ such as role locators or define an explicit testing contract using test ids.
 const todoInput = '.todo-input';
 const todoButton = '.todo-button';
 const todoItem = '.todo .todo-item';
+const todoItemCompleted = '.todo .completed';
+const todoCompleteButton = '.complete-btn';
+const todoFilter = '.filter-todo';
+const todoList = '.todo-list';
 
+// scenario1
 Given('a user has navigated to the homepage', async function() {
 	// navigate to the app
 	await page.goto(url)
@@ -48,3 +53,43 @@ Then('card {string} should be displayed on the webUI', async function(item) {
 
     //return 'pending';
     });
+
+// scenario2
+Given('a card {string} has been added to the todo list', async function(item)  {
+	// fill the item that was input from the feature file , to the inputText field in the UI
+	await page.fill(todoInput, item)
+	// click the button
+	await page.click(todoButton)    
+	});
+       
+When('the user marks the card {string} as completed using the webUI', async function(item) {
+	await page.click(todoCompleteButton)
+	});
+       
+Then('the card {string} should be labeled as completed on the webUI', async function(item) {
+	// Write code here that turns the phrase above into concrete actions
+	// get text of the item that has been marked as completed
+	const text = await page.innerText(todoItemCompleted)
+	// assert that its name is similar to what we provided
+	await expect(text).toBe(item)
+	//return 'pending';
+	});
+
+/*    
+Then('the card {string} shouldn\'t be displayed if filter uncomplete is applied', async function(item) {
+	// select filter uncomplete
+	//await page.selectOption(todoFilter, {value: "uncomplete"})
+	await page.selectOption(todoFilter, {value: "all"})
+	
+	const i = page.locator(todoItem).count();
+	await expect(i).toBe(0);
+	// check if card is still visible (iterate through all cards)
+	/*
+	for i in itemRoots
+		text = await page.innerText(todoItemCompleted)
+		await expect(text).not.toBe(item);
+		*/
+
+    //return 'pending';
+	//});
+
