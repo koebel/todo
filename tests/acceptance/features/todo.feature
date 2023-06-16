3ab308410da7e67394d247b88bb86ea77870278f
Kathrin Koebel <kathrin@koebel.ch>
2023-06-16 13:20:53 +0545

    clean up code

--- tests/acceptance/features/todo.feature
+++ tests/acceptance/features/todo.feature
@@ -12,7 +12,7 @@ Feature: todo
 		Given a user has navigated to the homepage 
 		And a card "new task" has been added to the todo list
 		When the user marks the card "new task" as completed using the webUI
-		Then the card "new task" should be labeled as completed on the webUI
+		Then card "new task" should be labeled as completed on the webUI
 
 	Scenario: delete todo list item
 		Given a user has navigated to the homepage 
@@ -24,9 +24,8 @@ Feature: todo
 		Given a user has navigated to the homepage 
 		And a card "task1" has been added to the todo list
 		And a card "task2" has been added to the todo list
-		And a card "task3" has been added to the todo list
-		And the card "task1" has been marked as completed
-		When the user sets the filter to "uncomplete" using the webUI
-		Then card "task2" should be displayed on the webUI
-		But card "task1" should not be displayed on the webUI
+		And card "task1" has been marked as completed
+		When the user sets the filter to "completed" using the webUI
+		Then card "task1" should be displayed on the webUI
+		But card "task2" should not be displayed on the webUI
 		
--- tests/acceptance/stepDefinitions/todoContext.js
+++ tests/acceptance/stepDefinitions/todoContext.js
@@ -1,13 +1,13 @@
 const {Given, When, Then} = require('@cucumber/cucumber')
 
 // import expect for assertion
-const { expect } = require("@playwright/test");
+const { expect } = require("@playwright/test")
 
 //launch url
 const url = 'http://localhost:3000'
 
 //define selectors
-const homepageElement = '.App' //'.borderTodo' 
+const homepageElement = '.App' // '.borderTodo' 
 /* 
 is it better to use class or id as selector? according to doc, both of them should be avoided... 
 see https://playwright.dev/docs/locators
@@ -15,63 +15,71 @@ CSS and XPath are not recommended as the DOM can often change leading to non res
 Instead, try to come up with a locator that is close to how the user perceives the page 
 such as role locators or define an explicit testing contract using test ids. 
 */
-const todoInput = '.todo-input';
-const todoButton = '.todo-button';
-const todoItem = '.todo .todo-item';
-const todoItemCompleted = '.todo .completed';
-const todoCompleteButton = '.complete-btn';
-const todoDeleteButton = '.trash-btn';
-const todoFilter = '.filter-todo';
-const todoList = '.todo-list';
+const todoInput = '.todo-input'
+const todoButton = '.todo-button'
+const todoItem = '.todo .todo-item'
+const todoItemCompleted = '.todo .completed'
+const todoCompleteButton = '.complete-btn'
+const todoDeleteButton = '.trash-btn'
+const todoFilter = '.filter-todo'
 
-// scenario1
+// initial methods for scenario1
 Given('a user has navigated to the homepage', async function() {
 	// navigate to the app
 	await page.goto(url)
 	// locate the element in the webUI
 	const locator = await page.locator(homepageElement)
-	// assert that it's visible
+	// assert that element is visible
 	await expect(locator).toBeVisible()
-    }); // is semicolon required? --> seems to be optional in JS
+    })
        
 When('the user adds {string} to the todo list using the webUI', async function(item) {
-	// fill the item that was input from the feature file , to the inputText field in the UI
+	// fill the item specified in the feature file into the inputText field in the UI
 	await page.fill(todoInput, item)
-	// click the button
+	// click the add todo button
 	await page.click(todoButton)
-    });
+    })
        
 Then('card {string} should be displayed on the webUI', async function(item) {
+    /*
     // get text of the item that is visible in the UI
 	const text = await page.innerText(todoItem)
-	// assert that its name is similar to what we provided
+	// assert that its name is equal to what we provided
 	await expect(text).toBe(item)
-    });
+	*/
 
-// scenario2
+	// alternative implementation
+	// search card with the label specified in the feature file
+	const locator = await page.locator(todoItem + ':text("' + item + '")')
+	// assert that this item is visible
+	await expect(locator).toBeVisible()
+    })
+
+// additional methods for scenario2
 Given('a card {string} has been added to the todo list', async function(item)  {
-	// fill the item that was input from the feature file , to the inputText field in the UI
+	// fill the item specified in the feature file into the inputText field in the UI
 	await page.fill(todoInput, item)
-	// click the button
+	// click the add todo button
 	await page.click(todoButton)    
-	});
+	})
        
 When('the user marks the card {string} as completed using the webUI', async function(item) {
+	// click the complete button
 	await page.click(todoCompleteButton)
-	});
+	})
        
-Then('the card {string} should be labeled as completed on the webUI', async function(item) {
-	// Write code here that turns the phrase above into concrete actions
-	// get text of the item that has been marked as completed
-	const text = await page.innerText(todoItemCompleted)
-	// assert that its name is similar to what we provided
-	await expect(text).toBe(item)
-	});
+Then('card {string} should be labeled as completed on the webUI', async function(item) {
+	// search card with the label specified in the feature file (card that has been marked as completed)
+	const locator = await page.locator(todoItemCompleted + ':text("' + item + '")')
+	// assert that this item is visible
+	await expect(locator).toBeVisible()
+	})
 
-//scenario3
+//additional methods for scenario3
 When('the user deletes the card {string} using the webUI', async function(item) {
+	// click the delete button
 	await page.click(todoDeleteButton)
-    });
+    })
 
 Then('card {string} should not be displayed on the webUI', async function(item) {
 	/*
@@ -80,16 +88,21 @@ Then('card {string} should not be displayed on the webUI', async function(item) 
 	ensure that there is no card with the given label
 	this test fails if there are two cards with the same label... it seems like label names don't need to be unique in this todo app...
 	*/
-	const locator = await page.locator(todoItem + ':text("' + item + '")')
-	await expect(locator).not.toBeVisible()
-	});
 
-//scenario4
-Given('the card {string} has been marked as completed', async function(item) {
+	// search card with the label specified in the feature file	
+	const locator = await page.locator(todoItem + ':text("' + item + '")')
+	// assert that this item is not visible
+	await expect(locator).not.toBeVisible()
+	})
+
+//additional methods for scenario4
+Given('card {string} has been marked as completed', async function(item) {
+	// click the complete button
 	await page.click(todoCompleteButton)
-    });
+    })
        
 When('the user sets the filter to {string} using the webUI', async function(item) {
+	// select filter speicified in the feature file
     await page.selectOption(todoFilter, {value: item})
-    });
+    })
 
